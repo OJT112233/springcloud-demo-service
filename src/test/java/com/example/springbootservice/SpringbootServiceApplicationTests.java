1aa8f4aed19169a5bffb14a242bf2f1ae54ada15
oujianteng <oujianteng@ly-sky.com>
2020-04-08 17:25:30 +0800

    提交代码测试

--- src/test/java/com/example/springbootservice/SpringbootServiceApplicationTests.java
+++ src/test/java/com/example/springbootservice/SpringbootServiceApplicationTests.java
@@ -1,11 +1,11 @@
-//package com.example.springbootservice;
-//
-//import org.junit.Test;
-//
-//public class SpringbootServiceApplicationTests {
-//
-//    @Test
-//    public void contextLoads() {
-//    }
-//
-//}
+package com.example.springbootservice;
+
+import org.junit.Test;
+
+public class SpringbootServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
